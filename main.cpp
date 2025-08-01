5f92f640868311152fb68259a58384eed655f130
Patrick H <minecraftbuilder2018@gmail.com>
2025-08-01 15:26:43 -0400

    More touchups. Further coming soon:tm:

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -10,32 +11,28 @@
 using OmegaInt = OmegaPoint<int, int, int>;
 using DecompArray = std::array<OmegaInt, 2>;
 
-void testing() {
+void inputByWords(const std::vector<std::string>& input) {
     ShadowGenerator gen;
-    std::vector<OmegaInt> points {{
-        { 2, -2, 1 }, { -1, 0, 2 }, { 1, 1, -1 }
-    }};
 
-    std::vector<OmegaInt> pointsTwo {{
-        { -1, 3, -1 }
-    }};
-
-    std::vector<OmegaInt> uniqueShadow {{
-            { -1, -2, 2 }
-    }};
-
-    gen.addPoints(uniqueShadow);
+    gen.addPoints(input);
     gen.generateShadow();
-    const std::vector<PointInfo>& shadowpoints { gen.getShadowAsPoints() };
-    // std::cout << shadowpoints.size() << '\n';
-    std::ofstream pointFile;
-    pointFile.open("points.txt");
-    for (auto i : shadowpoints) {
-        pointFile << i.point << '\n';
-    }
 
-    
+    const std::vector<PointInfo>& shadowpoints { gen.getShadowAsPoints() };
+
     std::ofstream file;
+    file.open("points.txt");
+
+    for (auto i : shadowpoints) {
+        file << i.point << '\n';
+    }
+    file.close();
+
+    file.open("inputPoints.txt");
+    for (std::size_t i = 4; i < 4 + input.size(); ++i) {
+        file << shadowpoints[i].point << '\n';
+    }
+    file.close();
+    
     file.open("words.txt");
     const std::vector<std::string>& words {gen.getShadowAsWords()};
     for (auto i = (words.begin()); i < words.end(); ++i) {
@@ -44,14 +41,37 @@ void testing() {
     file.close();
 }
 
-void convertToPoints() {
-    std::ifstream file;
-    file.open("~/Documents/Summer Research/Picture Code/points.txt");
+void inputByPoints(const std::vector<OmegaInt>& input) {
+    ShadowGenerator gen;
+
+    gen.addPoints(input);
+    gen.generateShadow();
+
+    const std::vector<PointInfo>& shadowpoints { gen.getShadowAsPoints() };
+
+    std::ofstream file;
+    file.open("points.txt");
+
+    for (auto i : shadowpoints) {
+        file << i.point << '\n';
+    }
+    file.close();
+
+    file.open("inputPoints.txt");
+    for (std::size_t i = 4; i < 4 + input.size(); ++i) {
+        file << shadowpoints[i].point << '\n';
+    }
+    file.close();
+    
+    file.open("words.txt");
+    const std::vector<std::string>& words {gen.getShadowAsWords()};
+    for (auto i = (words.begin()); i < words.end(); ++i) {
+        file << *i << '\n';
+    }
+    file.close();
 }
 
 int main() {
-    ShadowGenerator gen;
-
     std::vector<OmegaInt> points {{
         { -1, 3, -1 }
     }};
@@ -60,28 +80,8 @@ int main() {
         "stustsut"
     };
 
-    gen.addPoints(points);
-    gen.generateShadow();
+    inputByPoints(points);
+    // inputByWords(inputWords);
 
-    const std::vector<PointInfo>& shadowpoints { gen.getShadowAsPoints() };
-
-    std::ofstream pointFile;
-    pointFile.open("points.txt");
-
-    pointFile << shadowpoints.size() << '\n';
-    for (auto i : shadowpoints) {
-        pointFile << i.point << '\n';
-    }
-
-    
-    std::ofstream file;
-    file.open("words.txt");
-
-    const std::vector<std::string>& words {gen.getShadowAsWords()};
-    for (auto i = (words.begin()); i < words.end(); ++i) {
-        file << *i << '\n';
-    }
-    file.close();
-    
     return 0;
 }
